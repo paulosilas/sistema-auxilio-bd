80eff995e0903e318e075adec44d5f61d96ba2ac
renanluc <renan_luc93@hotmail.com>
2017-07-21 23:26:15 -0300

    Consertado problema da segunda conexão
    Foi alterado o método para criação da segunda conexão, evitando mensagens de erro com variáveis são criadas ou utilizadas.

--- conexao/conecta.php
+++ conexao/conecta.php
@@ -20,20 +20,20 @@
 
 		if(isset($_SESSION['nome_db'])){
 			$db_name = $_SESSION['nome_db'];
-
-			if($db_name != ""){
-				function conectaSegundo(){
-					try{
-						$db_name = $_SESSION['nome_db'];
-						$pdo2 = new PDO('mysql:host=localhost;dbname='.$db_name.';charset=utf8', 'root', '');
-					}catch(PDOException $e){
-						//var_dump($e);
-						echo $e->getMessage();
-					}
-					return $pdo2;
-				}
-			}
 		}
+
+
+		function conectaSegundo(){
+			try{
+				$db_name = $_SESSION['nome_db'];
+				$pdo2 = new PDO('mysql:host=localhost;dbname='.$db_name.';charset=utf8', 'root', '');
+			}catch(PDOException $e){
+				//var_dump($e);
+				echo $e->getMessage();
+			}
+				return $pdo2;
+			}
+		
 
 		/**
 		 * Cria o hash da senha, usando MD5 e SHA-1
