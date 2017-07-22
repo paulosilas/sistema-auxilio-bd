dd8e5085d0c9a1cd7dab27f0faf6ec1f504326bb
renanluc <renan_luc93@hotmail.com>
2017-07-21 23:21:00 -0300

    Alteração para desativar atividade
    Não é mais possível apagar as atividades do banco de dados desta forma, ela apenas é desativada e deixa de ser exibida na lista de atividades, impedindo que dados sejam perdidos no processo, como notas de atividades antigas.

--- delet_atividade.php
+++ delet_atividade.php
@@ -10,21 +10,11 @@
 	<div id="caixa">
 	<?php
 	if($con){
-		$sqlDelete = "SET FOREIGN_KEY_CHECKS=0;";
 
-		$resetaChave = $con->prepare($sqlDelete);
-		$resetaChave->execute();
-
-		$sql = "DELETE FROM atividade 
-		          WHERE cod_atividade = $cod_atividade;";	
+		$sql = "UPDATE atividade set status = 0 WHERE cod_atividade = $cod_atividade;";
 
 		$deleteAtividade = $con->prepare($sql);
 		$deleteAtividade->execute();
-
-		$sqlDelete2 = "SET FOREIGN_KEY_CHECKS=1;";
-
-		$ligaChave = $con->prepare($sqlDelete2);
-		$ligaChave->execute();
 
 		echo "<h1>Atividade excluida com sucesso.</h1>";
 		echo "<div id='redirect'><h3>Você será redirecionado em 3 Segundos... </h3></div>";
