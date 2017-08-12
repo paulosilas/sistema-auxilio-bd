9135bffd1f2ebf5c916f22fc2f7cbb210299d7a1
renanluc <renan_luc93@hotmail.com>
2017-08-12 16:12:02 -0300

    Alteração no Redirecionamento
    Alteração no Caminho do Redirecionamento

--- delet_bd.php
+++ delet_bd.php
@@ -30,7 +30,7 @@
 			echo "<h1>Banco Excluido com Sucesso!</h1>";
 			echo "<div id='redirect'><h3>Você será redirecionado em 3 Segundos... </h3></div>";
 	?>
-			<meta http-equiv="refresh" content=3;url="http://localhost:8088/template/bancos.php">
+			<meta http-equiv="refresh" content=3;url="/template/bancos.php">
 	<?php
 		}
 	
