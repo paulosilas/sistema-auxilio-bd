da6b88cfe149236409c9ecb97692d548e83ded94
renanluc <renan_luc93@hotmail.com>
2017-08-15 13:57:43 -0300

    adqs

--- insert_bd.php
+++ insert_bd.php
@@ -93,7 +93,7 @@
 					echo "<h1>Base de Dados Cadastrada com Sucesso.</h1>";
 					echo "<div id='redirect'><h3>Você será redirecionado em 3 Segundos... </h3></div>";
 					?>
-						<meta http-equiv="refresh" content=3;url="/template/bancos.php">
+						<meta http-equiv="refresh" content=3;url="/adqs/bancos.php">
 					<?php
 				}else{
 					echo "Falha ao inserir base de dados: ".mysql_error();
