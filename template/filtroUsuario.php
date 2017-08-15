4cf11333b449a95eb937b14be0566cfc1d08ab7e
renanluc <renan_luc93@hotmail.com>
2017-08-15 15:06:36 -0300

    Filtro para usuários
    Filtra os usuários cadastrados (professor ou aluno).

---
+++ template/filtroUsuario.php
@@ -0,0 +1,12 @@
+<div id="caixaFiltro">
+	<h3>Filtrar Usuários</h3>
+	<div id="filtro">	  
+	  <select id="primeiro">
+	    <?php
+			echo "<option value='-'>Usuários</option>";
+			echo "<option value='Professor'>Professor</option>";
+			echo "<option value='Aluno'>Aluno</option>";		
+		?>
+	  </select>
+	</div>
+</div>
