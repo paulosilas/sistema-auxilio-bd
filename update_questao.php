80f84a930c49d19ba0f33c0d1dc07476163b0b73
renanluc <renan_luc93@hotmail.com>
2017-08-15 14:06:13 -0300

    adqs

--- update_questao.php
+++ update_questao.php
@@ -21,7 +21,7 @@
 		echo "<h1>Questão atualizada com sucesso.</h1>";
 		echo "<div id='redirect'><h3>Você será redirecionado em 3 Segundos... </h3></div>";
 	?>
-		<meta http-equiv="refresh" content=3;url="/template/questoes.php">
+		<meta http-equiv="refresh" content=3;url="/adqs/questoes.php">
 	<?php
 
 	} else{
