02f3b46d56aac7c8e008a792ba1a90843694c98e
renanluc <renan_luc93@hotmail.com>
2017-08-16 22:37:33 -0300

    Redi adqs
    Alterado redirecionamento para adqs

--- delet_atividade.php
+++ delet_atividade.php
@@ -10,34 +10,14 @@
 	<div id="caixa">
 	<?php
 	if($con){
-		/*
-
-		$sqlDelete = "SET FOREIGN_KEY_CHECKS=0;";
-
-		$resetaChave = $con->prepare($sqlDelete);
-		$resetaChave->execute();
-
-		*/
-
 		$sql = "UPDATE atividade set status = 0 WHERE cod_atividade = $cod_atividade;";
-
-		//$sql = "DELETE FROM atividade WHERE cod_atividade = $cod_atividade;";	
-
 		$deleteAtividade = $con->prepare($sql);
 		$deleteAtividade->execute();
-
-		/*
-		$sqlDelete2 = "SET FOREIGN_KEY_CHECKS=1;";
-
-		$ligaChave = $con->prepare($sqlDelete2);
-		$ligaChave->execute();
-
-		*/
 
 		echo "<h1>Atividade excluida com sucesso.</h1>";
 		echo "<div id='redirect'><h3>Você será redirecionado em 3 Segundos... </h3></div>";
 	?>
-		<meta http-equiv="refresh" content=3;url="/template/atividades.php">
+		<meta http-equiv="refresh" content=3;url="/adqs/atividades.php">
 	<?php
 
 	
